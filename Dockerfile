4366623ae0951ad3bd74fccca5b09d02f3004860
tyaro <tkentyan@hotmail.com>
2021-07-07 20:38:13 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16-alpine3.11
+
+ENV LANG=C.UTF-8
+ENV TZ=Asia/Tokyo
+
+WORKDIR /usr/src/react-app
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose run --rm frontend sh -c 'npx create-react-app frontend --template typescript'
